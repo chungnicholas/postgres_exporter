b2880e3980ca1359f1b0595ac6e5a30b926bd2a5
Nicholas Chung <84615041+chungnicholas@users.noreply.github.com>
2023-07-05 16:51:12 +0800

    Comment out redundant queries

--- cmd/postgres_exporter/queries.go
+++ cmd/postgres_exporter/queries.go
@@ -47,110 +47,110 @@ type OverrideQuery struct {
 // TODO: validate this is a closed set in tests, and there are no overlaps
 var queryOverrides = map[string][]OverrideQuery{
 	"pg_stat_replication": {
-		{
-			semver.MustParseRange(">=10.0.0"),
-			`
-			SELECT *,
-				(case pg_is_in_recovery() when 't' then null else pg_current_wal_lsn() end) AS pg_current_wal_lsn,
-				(case pg_is_in_recovery() when 't' then null else pg_wal_lsn_diff(pg_current_wal_lsn(), pg_lsn('0/0'))::float end) AS pg_current_wal_lsn_bytes,
-				(case pg_is_in_recovery() when 't' then null else pg_wal_lsn_diff(pg_current_wal_lsn(), replay_lsn)::float end) AS pg_wal_lsn_diff
-			FROM pg_stat_replication
-			`,
-		},
-		{
-			semver.MustParseRange(">=9.2.0 <10.0.0"),
-			`
-			SELECT *,
-				(case pg_is_in_recovery() when 't' then null else pg_current_xlog_location() end) AS pg_current_xlog_location,
-				(case pg_is_in_recovery() when 't' then null else pg_xlog_location_diff(pg_current_xlog_location(), replay_location)::float end) AS pg_xlog_location_diff
-			FROM pg_stat_replication
-			`,
-		},
-		{
-			semver.MustParseRange("<9.2.0"),
-			`
-			SELECT *,
-				(case pg_is_in_recovery() when 't' then null else pg_current_xlog_location() end) AS pg_current_xlog_location
-			FROM pg_stat_replication
-			`,
-		},
+		//{
+		//	semver.MustParseRange(">=10.0.0"),
+		//	`
+		//	SELECT *,
+		//		(case pg_is_in_recovery() when 't' then null else pg_current_wal_lsn() end) AS pg_current_wal_lsn,
+		//		(case pg_is_in_recovery() when 't' then null else pg_wal_lsn_diff(pg_current_wal_lsn(), pg_lsn('0/0'))::float end) AS pg_current_wal_lsn_bytes,
+		//		(case pg_is_in_recovery() when 't' then null else pg_wal_lsn_diff(pg_current_wal_lsn(), replay_lsn)::float end) AS pg_wal_lsn_diff
+		//	FROM pg_stat_replication
+		//	`,
+		//},
+		//{
+		//	semver.MustParseRange(">=9.2.0 <10.0.0"),
+		//	`
+		//	SELECT *,
+		//		(case pg_is_in_recovery() when 't' then null else pg_current_xlog_location() end) AS pg_current_xlog_location,
+		//		(case pg_is_in_recovery() when 't' then null else pg_xlog_location_diff(pg_current_xlog_location(), replay_location)::float end) AS pg_xlog_location_diff
+		//	FROM pg_stat_replication
+		//	`,
+		//},
+		//{
+		//	semver.MustParseRange("<9.2.0"),
+		//	`
+		//	SELECT *,
+		//		(case pg_is_in_recovery() when 't' then null else pg_current_xlog_location() end) AS pg_current_xlog_location
+		//	FROM pg_stat_replication
+		//	`,
+		//},
 	},
 
 	"pg_replication_slots": {
-		{
-			semver.MustParseRange(">=9.4.0 <10.0.0"),
-			`
-			SELECT slot_name, database, active, pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn)
-			FROM pg_replication_slots
-			`,
-		},
-		{
-			semver.MustParseRange(">=10.0.0"),
-			`
-			SELECT slot_name, database, active, pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)
-			FROM pg_replication_slots
-			`,
-		},
+		//{
+		//	semver.MustParseRange(">=9.4.0 <10.0.0"),
+		//	`
+		//	SELECT slot_name, database, active, pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn)
+		//	FROM pg_replication_slots
+		//	`,
+		//},
+		//{
+		//	semver.MustParseRange(">=10.0.0"),
+		//	`
+		//	SELECT slot_name, database, active, pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)
+		//	FROM pg_replication_slots
+		//	`,
+		//},
 	},
 
 	"pg_stat_archiver": {
-		{
-			semver.MustParseRange(">=9.4.0"),
-			`
-			SELECT *,
-				extract(epoch from now() - last_archived_time) AS last_archive_age
-			FROM pg_stat_archiver
-			`,
-		},
+		//{
+		//	semver.MustParseRange(">=9.4.0"),
+		//	`
+		//	SELECT *,
+		//		extract(epoch from now() - last_archived_time) AS last_archive_age
+		//	FROM pg_stat_archiver
+		//	`,
+		//},
 	},
 
 	"pg_stat_activity": {
-		// This query only works
-		{
-			semver.MustParseRange(">=9.2.0"),
-			`
-			SELECT
-				pg_database.datname,
-				tmp.state,
-				tmp2.usename,
-				tmp2.application_name,
-				COALESCE(count,0) as count,
-				COALESCE(max_tx_duration,0) as max_tx_duration
-			FROM
-				(
-				  VALUES ('active'),
-				  		 ('idle'),
-				  		 ('idle in transaction'),
-				  		 ('idle in transaction (aborted)'),
-				  		 ('fastpath function call'),
-				  		 ('disabled')
-				) AS tmp(state) CROSS JOIN pg_database
-			LEFT JOIN
-			(
-				SELECT
-					datname,
-					state,
-					usename,
-					application_name,
-					count(*) AS count,
-					MAX(EXTRACT(EPOCH FROM now() - xact_start))::float AS max_tx_duration
-				FROM pg_stat_activity GROUP BY datname,state,usename,application_name) AS tmp2
-				ON tmp.state = tmp2.state AND pg_database.datname = tmp2.datname
-			`,
-		},
-		{
-			semver.MustParseRange("<9.2.0"),
-			`
-			SELECT
-				datname,
-				'unknown' AS state,
-				usename,
-				application_name,
-				COALESCE(count(*),0) AS count,
-				COALESCE(MAX(EXTRACT(EPOCH FROM now() - xact_start))::float,0) AS max_tx_duration
-			FROM pg_stat_activity GROUP BY datname,usename,application_name
-			`,
-		},
+		//// This query only works
+		//{
+		//	semver.MustParseRange(">=9.2.0"),
+		//	`
+		//	SELECT
+		//		pg_database.datname,
+		//		tmp.state,
+		//		tmp2.usename,
+		//		tmp2.application_name,
+		//		COALESCE(count,0) as count,
+		//		COALESCE(max_tx_duration,0) as max_tx_duration
+		//	FROM
+		//		(
+		//		  VALUES ('active'),
+		//		  		 ('idle'),
+		//		  		 ('idle in transaction'),
+		//		  		 ('idle in transaction (aborted)'),
+		//		  		 ('fastpath function call'),
+		//		  		 ('disabled')
+		//		) AS tmp(state) CROSS JOIN pg_database
+		//	LEFT JOIN
+		//	(
+		//		SELECT
+		//			datname,
+		//			state,
+		//			usename,
+		//			application_name,
+		//			count(*) AS count,
+		//			MAX(EXTRACT(EPOCH FROM now() - xact_start))::float AS max_tx_duration
+		//		FROM pg_stat_activity GROUP BY datname,state,usename,application_name) AS tmp2
+		//		ON tmp.state = tmp2.state AND pg_database.datname = tmp2.datname
+		//	`,
+		//},
+		//{
+		//	semver.MustParseRange("<9.2.0"),
+		//	`
+		//	SELECT
+		//		datname,
+		//		'unknown' AS state,
+		//		usename,
+		//		application_name,
+		//		COALESCE(count(*),0) AS count,
+		//		COALESCE(MAX(EXTRACT(EPOCH FROM now() - xact_start))::float,0) AS max_tx_duration
+		//	FROM pg_stat_activity GROUP BY datname,usename,application_name
+		//	`,
+		//},
 	},
 }
 
